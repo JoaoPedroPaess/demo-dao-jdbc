1cbcb404ee87065c867d799c1d76cfb105e680ee
João Paes <126814093+xuizi@users.noreply.github.com>
2024-09-04 14:41:41 -0300

    SellerDAO e DepartmentDAO

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,15 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+import model.entities.seller;
+
 public interface DepartmentDao {
 
+	void insert(seller obj);
+	void update(seller obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<seller> findAll();
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,16 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+import model.entities.seller;
+
 public interface SellerDao {
+
+	void insert(seller obj);
+	void update(seller obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<seller> findAll();
 
 }
