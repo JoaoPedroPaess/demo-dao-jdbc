479b4fe605aa0c0fb038bfd17cbc11f81f91bd70
João Paes <126814093+xuizi@users.noreply.github.com>
2024-09-04 14:27:43 -0300

    Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "books");
+		seller seller2 = new seller(21, "bob","bob@", new Date(), 3000.0, obj);
+		System.out.println(seller2);
 		System.out.println(obj);
 
 	}
